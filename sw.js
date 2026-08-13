691fe37e244fa0bed9c300c261e84b5583069b98
ahmed0soliman6 <3.01318054e+08+ahmed0soliman6@users.noreply.github.com>
2026-08-13 16:06:44 +0000

    Deploy Soli Medical PWA to GitHub Pages

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "soli-medical-github-pages-v2";
+const BASE_URL = new URL("./", self.registration.scope);
+const APP_SHELL = [
+  new URL("./", BASE_URL).toString(),
+  new URL("./manifest.webmanifest", BASE_URL).toString(),
+  new URL("./assets/medicenter-icon.png", BASE_URL).toString(),
+  new URL("./assets/medicenter-splash.png", BASE_URL).toString()
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) return;
+  event.respondWith(caches.match(event.request).then((cachedResponse) => {
+    const networkResponse = fetch(event.request).then((response) => {
+      if (response.ok && response.type === "basic") {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => cachedResponse || caches.match(new URL("./", BASE_URL).toString()));
+    return cachedResponse || networkResponse;
+  }));
+});
